fcb3e943337c689c1530a41b0ac511a861b665cf
DmitryEliseev <dima-eliseev10@rambler.ru>
2019-10-19 14:48:51 +0300

    Updating code to do exercise

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This is homework exercise for Coursera course 
+## "Data Science: Foundations using R" by Johns Hopkins University
+## URL: https://www.coursera.org/learn/r-programming/home/welcome
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Create object with caching functional
+  
+  inversed_matrix <- NULL
+  set <- function(y) {
+    x <<- y
+    inversed_matrix <<- NULL
+  }
+  get <- function() x
+  set_inverse <- function(inverse) inversed_matrix <<- inverse
+  get_inverse <- function() inversed_matrix
+  list(set = set, get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Calculate an inverse of matrix 'x'
+  ## Return the inverse from cache if it was stored there
+  
+  matrix <- x$get_inverse()
+  if(!is.null(matrix)) {
+    message("getting cached data")
+    return(matrix)
+  }
+  data <- x$get()
+  inversed_matrix <- solve(data)
+  x$set_inverse(inversed_matrix)
+  inversed_matrix
 }
